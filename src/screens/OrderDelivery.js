1053b8e3128066a5521c966bb71217da700fde2c
DevABU <aboudallaa@qualitiz.com>
2021-04-03 23:53:27 +0100

    setting up screens

---
+++ src/screens/OrderDelivery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function OrderDelivery () {
+    return (
+        <View>
+            <Text>Order Delivery</Text>
+        </View>
+    );
+}
+
+export default OrderDelivery;
